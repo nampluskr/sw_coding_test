d709b69e71285cfb86fde47618c9a72eef446d9d
nampluskr <nampluskr@gmail.com>
2024-06-30 17:43:19 +0900

    update

--- sw_pro_2021/210903_AI로봇/depq.h
+++ sw_pro_2021/210903_AI로봇/depq.h
@@ -7,7 +7,8 @@ using namespace std;
 template<typename T>
 struct DoubleEnededPQ {
     struct Pair {
-        T data; int idx;
+        T data;
+        int idx;
         bool operator<(const Pair& p) const { return data < p.data; }
         bool operator>(const Pair& p) const { return data > p.data; }
     };
@@ -15,38 +16,41 @@ struct DoubleEnededPQ {
     priority_queue<Pair, vector<Pair>, greater<Pair>> minHeap;
     vector<bool> popped;
     int idx = 0;
+    int heapSize = 0;
 
     void clear() {
         maxHeap = {};
         minHeap = {};
         popped.clear();
         idx = 0;
+        heapSize = 0;
     }
     void push(const T& data) {
         maxHeap.push({ data, idx });
         minHeap.push({ data, idx });
         popped.push_back(false);
         idx++;
+        heapSize++;
     }
-    T popMax() {
+    void popMax() {
         while (!maxHeap.empty() && popped[maxHeap.top().idx]) maxHeap.pop();
-        auto top = maxHeap.top(); maxHeap.pop();
-        popped[top.idx] = true;
-        return top.data;
+        auto max = maxHeap.top(); maxHeap.pop();
+        popped[max.idx] = true;
+        heapSize--;
     }
-    T popMin() {
+    void popMin() {
         while (!minHeap.empty() && popped[minHeap.top().idx]) minHeap.pop();
-        auto bot = minHeap.top(); minHeap.pop();
-        popped[bot.idx] = true;
-        return bot.data;
+        auto min = minHeap.top(); minHeap.pop();
+        popped[min.idx] = true;
+        heapSize--;
     }
-    // T top() {
-    //     while (!maxHeap.empty() && popped[maxHeap.top().idx]) maxHeap.pop();
-    //     return maxHeap.top().data;
-    // }
-    // T bottom() {
-    //     while (!minHeap.empty() && popped[minHeap.top().idx]) minHeap.pop();
-    //     return minHeap.top().data;
-    // }
-    bool empty() { return idx == 0;}
+    T max() {
+        while (!maxHeap.empty() && popped[maxHeap.top().idx]) maxHeap.pop();
+        return maxHeap.top().data;
+    }
+    T min() {
+        while (!minHeap.empty() && popped[minHeap.top().idx]) minHeap.pop();
+        return minHeap.top().data;
+    }
+    bool empty() { return heapSize == 0;}
 };
---
+++ sw_pro_2021/210903_AI로봇/user_nam_min_max_heap.cpp
@@ -0,0 +1,119 @@
+#if 1
+#include <vector>
+#include <queue>
+using namespace std;
+
+#define MAX_ROBOTS  50'001
+#define MAX_WORKS   50'001
+
+#define TRAINING    0
+#define BROKEN      1
+#define WORKING     2
+
+struct Robot {
+    int wID, startTime, workTime, state;
+} robots[MAX_ROBOTS];
+
+vector<int> robotList[MAX_WORKS];
+
+// IQ => MAX_WORKTIME - workTime (IQ 는 workTime 에 반비례)
+struct Data {
+    int rID, workTime;
+
+    // maxHeap: IQ 클수록, rID 작을수록
+    bool operator<(const Data& data) const {
+        return (workTime > data.workTime) || (workTime == data.workTime && rID > data.rID);
+    }
+    // minHeap: IQ 작을수록, rID 작을수록
+    bool operator>(const Data& data) const {
+        return (workTime < data.workTime) || (workTime == data.workTime && rID > data.rID);
+    }
+};
+priority_queue<Data, vector<Data>, less<Data>> maxHeap;
+priority_queue<Data, vector<Data>, greater<Data>> minHeap;
+
+
+/////////////////////////////////////////////////////////////////////
+void init(int N)
+{
+    minHeap = {};
+    maxHeap = {};
+
+    for (int i = 0; i < MAX_WORKS; i++) {
+        robotList[i].clear();
+    }
+    for (int rID = 1; rID <= N; rID++) {
+        robots[rID].state = TRAINING;
+        robots[rID].workTime = 0;
+
+        maxHeap.push({ rID, robots[rID].workTime });
+        minHeap.push({ rID, robots[rID].workTime });
+    }
+}
+
+int callJob(int cTime, int wID, int mNum, int mOpt)
+{
+    int cnt = 0;
+    int res = 0;
+
+    while (cnt < mNum && !maxHeap.empty() && !minHeap.empty()) {
+        Data data;
+        if (mOpt == 0) { data = maxHeap.top(); maxHeap.pop(); }
+        if (mOpt == 1) { data = minHeap.top(); minHeap.pop(); }
+        int rID = data.rID;
+
+        if (robots[rID].state != TRAINING) continue;
+        if (robots[rID].workTime != data.workTime) continue;
+
+        robots[rID].state = WORKING;
+        robots[rID].wID = wID;
+        robots[rID].startTime = cTime;
+        robotList[wID].push_back(rID);
+
+        res += rID;
+        cnt++;
+    }
+    return res;
+}
+
+void returnJob(int cTime, int wID)
+{
+    for (int rID: robotList[wID]) {
+        if (robots[rID].wID != wID) continue;
+        if (robots[rID].state == WORKING) {
+            robots[rID].state = TRAINING;
+            robots[rID].workTime += (cTime - robots[rID].startTime);
+
+            maxHeap.push({ rID, robots[rID].workTime });
+            minHeap.push({ rID, robots[rID].workTime });
+        }
+    }
+}
+
+void broken(int cTime, int rID)
+{
+    if (robots[rID].state == WORKING)
+        robots[rID].state = BROKEN;
+}
+
+void repair(int cTime, int rID)
+{
+    if (robots[rID].state == BROKEN) {
+        robots[rID].state = TRAINING;
+        robots[rID].workTime = cTime;
+
+        maxHeap.push({ rID, robots[rID].workTime });
+        minHeap.push({ rID, robots[rID].workTime });
+    }
+}
+
+int check(int cTime, int rID)
+{
+    if (robots[rID].state == BROKEN)
+        return 0;
+    else if (robots[rID].state == WORKING)
+        return -robots[rID].wID;
+    else
+        return cTime - robots[rID].workTime;
+}
+#endif
