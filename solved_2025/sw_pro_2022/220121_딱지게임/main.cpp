73450b986fbafaa8cdd51281e672ddef590452a0
nampluskr <nampluskr@gmail.com>
2025-01-26 21:52:34 +0900

    update

---
+++ solved_2025/sw_pro_2022/220121_딱지게임/main.cpp
@@ -0,0 +1,82 @@
+﻿#ifndef _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+
+#include <stdio.h>
+#include <time.h>
+
+extern void init(int, int);
+extern int addDdakji(int, int, int, int);
+extern int check(int, int);
+
+static unsigned int seed = 5;
+static int pseudo_rand(void)
+{
+    seed = seed * 214013 + 2531011;
+    return (seed >> 16) & 0x7FFF;
+}
+
+static int board_size, ddakji_size, query_cnt;
+
+static int run(int score)
+{
+    scanf("%d%d%d%d", &seed, &board_size, &ddakji_size, &query_cnt);
+    init(board_size, ddakji_size);
+
+    for (int q = 0; q < query_cnt; q++)
+    {
+        int r, c, size, user_ans, correct_ans;
+
+        size = (pseudo_rand() * pseudo_rand()) % ddakji_size + 1;
+        r = (pseudo_rand() * pseudo_rand()) % (board_size - size + 1);
+        c = (pseudo_rand() * pseudo_rand()) % (board_size - size + 1);
+
+        user_ans = addDdakji(r, c, size, 1);
+        scanf("%d", &correct_ans);
+
+        if (user_ans != correct_ans)
+        score = 0;
+
+        size = (pseudo_rand() * pseudo_rand()) % ddakji_size + 1;
+        r = (pseudo_rand() * pseudo_rand()) % (board_size - size + 1);
+        c = (pseudo_rand() * pseudo_rand()) % (board_size - size + 1);
+
+        user_ans = addDdakji(r, c, size, 2);
+        scanf("%d", &correct_ans);
+
+        if (user_ans != correct_ans)
+            score = 0;
+
+        r = (pseudo_rand() * pseudo_rand()) % (board_size);
+        c = (pseudo_rand() * pseudo_rand()) % (board_size);
+
+        user_ans = check(r, c);
+        scanf("%d", &correct_ans);
+
+        if (user_ans != correct_ans)
+            score = 0;
+    }
+
+    return score;
+}
+
+int main()
+{
+    clock_t start = clock();
+    setbuf(stdout, NULL);
+    freopen("sample_input.txt", "r", stdin);
+
+    int tc, score;
+    scanf("%d%d", &tc, &score);
+
+    for (int t = 1; t <= tc; t++)
+    {
+        clock_t t_start = clock();
+        int t_score = run(score);
+        int t_result = (clock() - t_start) / (CLOCKS_PER_SEC / 1000);
+        printf("#%2d %d (%3d ms)\n", t, score, t_result);
+    }
+    int result = (clock() - start) / (CLOCKS_PER_SEC / 1000);
+    printf(">> Result: %d ms\n", result);
+    return 0;
+}
---
+++ solved_2025/sw_pro_2022/220121_딱지게임/user_stl.cpp
@@ -0,0 +1,43 @@
+﻿#if 1       // xx ms (STL)
+#include <vector>
+
+#define MAX_DDAKJIS 20'000  // 각 테스트 케이스는 최대 10,000 턴 진행
+#define MAX_BUCKETS 10'001  // 파티션 최대 개수
+
+struct Ddakji {
+    int mRow, mCol, mSize, mPlayer;
+} ddakjis[MAX_DDAKJIS];
+int ddakjiCnt;
+
+int bucketSize;
+int bucketCnt;
+std::vector<int> buckets[MAX_BUCKETS][MAX_BUCKETS];
+
+int N;  // 게임 판의 한 변의 길이 (10 ≤ N ≤ 100,000,000)
+int M;  // 딱지의 한 변의 길이의 최대 값 (1≤ M ≤ 10,000,000)
+
+//////////////////////////////////////////////////////////////////////
+void init(int N, int M)
+{
+    ::N = N, ::M = M;
+    ddakjiCnt = 0;
+
+    bucketSize = sqrt(N);
+    bucketCnt = ceil((double)N / bucketSize);
+    for (int i = 0; i <= N / bucketSize; i++)
+        for (int j = 0; j <= N / bucketSize; j++)
+            buckets[i][j].clear();
+}
+
+int addDdakji(int mRow, int mCol, int mSize, int mPlayer)
+{
+
+
+ return 0;
+}
+
+int check(int mRow, int mCol)
+{
+ return 0;
+}
+#endif
